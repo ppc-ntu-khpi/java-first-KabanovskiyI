b5b2eca569561a5f71bd48c8a70a8740881b9299
KabanovskiyI <160606334+KabanovskiyI@users.noreply.github.com>
2025-02-05 19:33:14 +0000

    done

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  String quote = "That which does not kill us makes us stronger.";
+  String owner = "Friedrich Nietzsche";
+  public void display() {
+    System.out.println(quote);
+    System.out.println(owner);
+  }
+}
